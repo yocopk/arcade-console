5bfa669505e59921dd2e27b014e530e8c95bf60c
yocopk <yocopk@gmail.com>
2023-12-05 13:00:31 +0100

    update

--- testa-croce/testa-croce.js
+++ testa-croce/testa-croce.js
@@ -2,23 +2,21 @@ const coinIcon = document.getElementById("coin");
 const tossBtn = document.getElementById("btn");
 const result = document.getElementById("result");
 tossBtn.addEventListener("click", () => {
-    tossBtn.disabled = true;
-    tossCoinFunction();
+  tossBtn.disabled = true;
+  tossCoinFunction();
 });
 
 function tossCoinFunction() {
-    const randomVal = Math.random();
-    const faceCoin = randomVal < 0.5 ? "Testa" : "Croce";
-    const imageUrl = faceCoin === "Testa" ?
-        "../assets/testa.jpg" :
-        "../assets/croce.jpg";
+  const randomVal = Math.random();
+  const faceCoin = randomVal < 0.5 ? "Testa" : "Croce";
+  const imageUrl =
+    faceCoin === "Testa" ? "../assets/testa.png" : "../assets/croce.png";
 
-    coinIcon.classList.add("flip");
-    setTimeout(() => {
-        coinIcon.innerHTML =
-            `<img src="${imageUrl}" alt="${faceCoin}">`;
-        coinIcon.classList.remove("flip");
-        result.textContent = `${faceCoin}`;
-        tossBtn.disabled = false;
-    }, 1000);
+  coinIcon.classList.add("flip");
+  setTimeout(() => {
+    coinIcon.innerHTML = `<img src="${imageUrl}" alt="${faceCoin}">`;
+    coinIcon.classList.remove("flip");
+    result.textContent = `${faceCoin}`;
+    tossBtn.disabled = false;
+  }, 1000);
 }
