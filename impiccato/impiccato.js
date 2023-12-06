a264a9b861b2c8ec92c2fd546f7bdb126b50c719
RolfoRouge <rossirosdolfo@gmail.com>
2023-12-06 08:18:30 +0000

    commenti

--- impiccato/impiccato.js
+++ impiccato/impiccato.js
@@ -12,6 +12,7 @@ let bottoni = document.getElementsByClassName("lettera-alfabeto");
 
 let punteggio = 0;
 let lettereGuessate = 0;
+let conteggioErrore = 0;
 
 let parolaDaIndovinare = [
   "VEDOVA",
@@ -54,121 +55,102 @@ for (let i = 0; i < lunghezzaParola; i++) {
 }
 
 
-let conteggioErrore = 0;
-
-// invio.addEventListener("click", function(){
-//   let parolaInserita = tentativo.value.split("");
-//   let letteraTrovata = false;
-//   // console.log(parolaInserita);
-//   for ( let i= 0; i < parolaInserita.length; i++ ){
-
-//     for( let y = 0 ; y < parolaSplittata.length; y++ ){
-
-//       if( parolaInserita[i] == parolaSplittata[y]) {
-//         document.getElementById(y).classList.remove("hidden"); 
-//         letteraTrovata = true;
-//       } 
-//     }
-
-//     if( letteraTrovata == false ) {
-//       conteggioErrore++;
-
-//     } else {
-//       letteraTrovata = false;
-//     }
-//   }
-
-
-
-  
-//   visualizzaErrori.innerText = "numero di errori:" + conteggioErrore;
-//   tentativo.value = "";
-
-// })
-
+// funzione che governa l'interazione delle lettere
 function premiLettera (lettera, bottone) {
 
-    let letteraTrovata = false;
-    // bottone.removeAttribute("onclick");
+  let letteraTrovata = false;
+  // bottone.removeAttribute("onclick");
   
-    for( let y = 0 ; y < parolaSplittata.length; y++ ){
-      if( lettera == parolaSplittata[y]) {
-        document.getElementById(y).classList.remove("hidden-lettera"); 
-        lettereGuessate ++;
+  for( let y = 0 ; y < parolaSplittata.length; y++ ){
+    if( lettera == parolaSplittata[y]) {
+      document.getElementById(y).classList.remove("hidden-lettera"); 
+      lettereGuessate ++;
 
-        if (lettereGuessate == parolaSplittata.length ) {
-            lettereGuessate = 0;
-            youWin.classList.remove("hidden");
-            visualizzaErrori.classList.add("hidden");
-            tastiera.classList.add("hidden");
-            punteggio++;
-            visualizzaPunteggio[1].innerText = "punteggio: " + punteggio;   
-        }
-
-        letteraTrovata = true;
-      } 
-    }
-  
-    if( !letteraTrovata ) {
-      // errore
-      conteggioErrore ++;
-      conteggioErrore = haiPerso(conteggioErrore);
-    }
-
-    bottone.disabled = true;
-}
-
-function haiPerso(errori) {
-
-    if (errori < 6) {
-        visualizzaErrori.innerText = "numero di errori:" + conteggioErrore;
-        return errori;
-    } else {
+      if (lettereGuessate == parolaSplittata.length ) {
 
         lettereGuessate = 0;
-        gameOver.classList.remove("hidden");
-        tastiera.classList.add("hidden");
+        youWin.classList.remove("hidden");
         visualizzaErrori.classList.add("hidden");
+        tastiera.classList.add("hidden");
+        punteggio++;
+        visualizzaPunteggio[1].innerText = "punteggio: " + punteggio;   
 
-        for( let y = 0 ; y < parolaSplittata.length; y++ ){
-            if( document.getElementById(y).classList.contains("hidden-lettera")) {
-                document.getElementById(y).classList.remove("hidden-lettera", "lettera"); 
-                document.getElementById(y).classList.add("lettera-non-indovinata"); 
-            } 
-        }
+      }
 
-        visualizzaPunteggio[0].innerText = "punteggio: " + punteggio;
-        punteggio = 0;
+      letteraTrovata = true;
+    } 
+  }
+  
+  // gestione errori
+  if( !letteraTrovata ) {
+    conteggioErrore ++;
+    conteggioErrore = haiPerso(conteggioErrore);
+  }
 
-        return 0;
-        
-    }
+  // diattiviamo il bottone dopo l'uso
+  bottone.disabled = true;
+
 }
 
+// funzione se viene commesso un errore o si perde
+function haiPerso(errori) {
+
+  if (errori < 6) {
+    visualizzaErrori.innerText = "numero di errori:" + conteggioErrore;
+    return errori;
+  } else {
+
+  lettereGuessate = 0;
+  gameOver.classList.remove("hidden");
+  tastiera.classList.add("hidden");
+  visualizzaErrori.classList.add("hidden");
+
+  for( let y = 0 ; y < parolaSplittata.length; y++ ){
+    if( document.getElementById(y).classList.contains("hidden-lettera")) {
+      document.getElementById(y).classList.remove("hidden-lettera", "lettera"); 
+      document.getElementById(y).classList.add("lettera-non-indovinata"); 
+    } 
+  }
+
+  visualizzaPunteggio[0].innerText = "punteggio: " + punteggio;
+  punteggio = 0;
+
+  return 0;
+        
+  }
+
+}
+
+// funzione che prende una parola casuale
 function parolaCasuale() {
     return Math.round(Math.random()* (parolaDaIndovinare.length-1));
 }
 
 function rigioca(vittoria) {
 
+  // maschero la schermata di fine
     if (vittoria) {
         youWin.classList.add("hidden");
     } else {
         gameOver.classList.add("hidden");
     }
 
+    // azzero i conteggi e la pagina
+    conteggioErrore = 0;
     visualizzaErrori.classList.remove("hidden");
     visualizzaErrori.innerText = "";
     tastiera.classList.remove("hidden");
 
+    // RESET PAROLA NASCOSTA
     parolaNascosta.innerHTML = "";
 
     parolaSplittata = parolaDaIndovinare[parolaCasuale()].split("");
-    console.log(parolaSplittata);
+    // console.log(parolaSplittata);
     
     lunghezzaParola = parolaSplittata.length;
-    console.log(lunghezzaParola);
+    // console.log(lunghezzaParola);
     
+    // Costruzione nuova parola nascosta
     for (let i = 0; i < lunghezzaParola; i++) {
     
       let nuovaDiv = document.createElement("div");
@@ -178,6 +160,7 @@ function rigioca(vittoria) {
       parolaNascosta.appendChild(nuovaDiv);
     } 
 
+    // RESET DEI TASTI
     for (let i = 0; i<26; i++) {
         // console.log(i);
         bottoni[i].disabled = false;
